e20092cd615429cfee46c106d641bb6a91d4730f
santiagomendezcar-oss <santiagomendezcar@gmail.com>
2026-04-26 13:41:35 -0500

    tegundo Cambio

--- src/main/java/com/example/backend/Configuration/CorsConfigurer.java
+++ src/main/java/com/example/backend/Configuration/CorsConfigurer.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfigurer {
 
-    @Bean(name = "customCorsConfigurer")  // ← Cambiado el nombre
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://10.0.2.2:8080")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
